b0938df11f1d2c31123739ecd531e2dc215ea7cd
jpschoen <jpschoen@gmail.com>
2018-07-03 13:35:51 -0500

    added block setting

--- DIC_functions/DIC_SBM.R
+++ DIC_functions/DIC_SBM.R
@@ -15,7 +15,7 @@ marriage_m <- as.matrix.network(flomarriage,matrix.type="adjacency")
 business_m <- as.matrix.network(flobusiness,matrix.type="adjacency")
 
 
-model1 <- CID.Gibbs(marriage_m, draws = 10000, burnin = 10000,
+model1 <- CID.Gibbs(marriage_m, components=c (SBM(2)), draws = 1000, burnin = 1000,
               thin = 5)
 
 plot(model1)
